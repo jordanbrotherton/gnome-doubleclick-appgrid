38e23b9b5cd8fd8e3c296d9fc19bea84b24b0cfc
Jordan Brotherton <135299181+jordanbrotherton@users.noreply.github.com>
2026-02-16 20:02:18 -0500

    init: Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Double Click Activities to App Grid
+Open the application grid by double clicking the activities button similarly to the super key.
+
+## Supported GNOME versions.
+So far, this extension only supports GNOME 49. GNOME 50 support should be soon whenever I can test it.
---
+++ extension.js
@@ -0,0 +1,27 @@
+import Clutter from 'gi://Clutter';
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import * as OverviewControls from 'resource:///org/gnome/shell/ui/overviewControls.js';
+
+export default class DoubleClickApps {
+    enable() {
+        this._doubleClickGesture = new Clutter.ClickGesture();
+        this._doubleClickGesture.set_n_clicks_required(2)
+
+        this._doubleClickGesture.connect('recognize', () => {
+          if (Main.overview.visible) {
+              const checked = Main.overview.dash.showAppsButton.checked;
+              Main.overview.dash.showAppsButton.checked = !checked;
+          } else {
+              Main.overview.show(OverviewControls.ControlsState.APP_GRID);
+          }
+        });
+        Main.panel.statusArea['activities']?.add_action(this._doubleClickGesture);
+    }
+
+    disable() {
+        if (this._doubleClickGesture) {
+            Main.panel.statusArea['activities']?.remove_action(this._doubleClickGesture);
+            delete this._doubleClickGesture;
+        }
+    }
+}
